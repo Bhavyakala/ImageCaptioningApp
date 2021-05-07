d505ff266d9527f4502eb85d459aa8e9e967488a
Bhavya Kala <bhavya.kala10@gmail.com>
2021-05-07 14:50:09 +0530

    test for file upload added

--- backend/image_caption/image_caption/settings.py
+++ backend/image_caption/image_caption/settings.py
@@ -124,3 +124,7 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+import sys
+
+TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
--- backend/image_caption/main/tests.py
+++ backend/image_caption/main/tests.py
@@ -3,22 +3,30 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.core.files.uploadedfile import InMemoryUploadedFile, SimpleUploadedFile
 import os
+from shutil import rmtree
 from django.conf import settings
 from PIL import Image
 
 basedir = settings.BASE_DIR
+if settings.TESTING:
+  settings.MEDIA_ROOT +='test'
+
 # Create your tests here.
 
 class InferenceUploadTest(APITestCase):
+
+
+  def tearDown(self):
+    rmtree(settings.MEDIA_ROOT, ignore_errors=True)
+
 
   def test_file_is_accepted(self):
 
     """
       Test for file uploads
     """
-    # print(basedir)
+
     file = open(os.path.join(basedir,'main/testImages/test1.jpeg') , 'rb')
     f = SimpleUploadedFile("D:/Github/ImageCaptioningApp/backend/image_caption/main/testImages/test1.jpeg",file.read(),content_type='image/jpeg')
     response = self.client.post('http://127.0.0.1:8000/image_upload/',data={'image':f},format='multipart')
-    # print(response.content)
     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
